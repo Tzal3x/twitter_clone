97395241052ac509bacdea8b569223624052b2a0
georgesouliotis <george_astrobip96@hotmail.com>
2023-05-29 12:41:36 +0300

    Initialize our Db blueprint with alembic migration tool

---
+++ migrations/versions/83de276c1b8f_create_follows_table.py
@@ -0,0 +1,33 @@
+"""Create follows table
+
+Revision ID: 83de276c1b8f
+Revises: f7bc16aee4fb
+Create Date: 2023-05-29 11:52:58.610494
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '83de276c1b8f'
+down_revision = 'f7bc16aee4fb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table('follows',
+    sa.Column('follower_id', sa.Integer(), nullable=False),
+    sa.Column('followee_id', sa.Integer(), nullable=False),
+    sa.Column('is_active', sa.Boolean(), server_default=True, nullable=False),
+    sa.Column('created_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
+    sa.Column('updated_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
+    sa.ForeignKeyConstraint(['followee_id'], ['users.id'], ),
+    sa.ForeignKeyConstraint(['follower_id'], ['users.id'], ),
+    sa.PrimaryKeyConstraint('follower_id', 'followee_id')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('follows')
