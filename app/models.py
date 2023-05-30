a2ef7a28f50cb9289923e8a387be6d96aa11cfa4
georgesouliotis <george_astrobip96@hotmail.com>
2023-05-30 20:31:55 +0300

    Update app models

--- app/models.py
+++ app/models.py
@@ -2,8 +2,10 @@
 from datetime import datetime
 
 from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 from sqlalchemy import (
-    Column, Integer, String, DateTime, ForeignKey
+    Column, ForeignKey, Unicode, UnicodeText, Integer, String, DateTime, Boolean
     )
 
 
@@ -14,35 +16,47 @@ class Users(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False)
-    first_name = Column(String)
-    last_name = Column(String)
+    username = Column(Unicode, nullable=False)
+    first_name = Column(Unicode)
+    last_name = Column(Unicode)
     birth = Column(DateTime)
-    created = Column(DateTime, default=datetime.utcnow)
+    email = Column(String(254), nullable=False) # ToDo - It should be unique
+    phone_number = Column(String(15), nullable=False)
+    bio = Column(Unicode(160), nullable=True)
+    is_active = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    
     tweets = relationship("Tweets", back_populates="user")
     comments = relationship("Comments", back_populates="user")
 
 
 class Tweets(Base):
     __tablename__ = 'tweets'
+    
     id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    created = Column(DateTime, default=datetime.utcnow)
-    updated = Column(DateTime, default=datetime.utcnow)
-
+    title = Column(Unicode, nullable=False)
+    description = Column(UnicodeText, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
+    is_active = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    
     user = relationship("Users", back_populates="tweets")
     comments = relationship("Comments", back_populates="tweet")
 
 
 class Comments(Base):
     __tablename__ = 'comments'
+    
     id = Column(Integer, primary_key=True)
-    body = Column(String, nullable=False)
-
+    body = Column(Unicode, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
     tweet_id = Column(Integer, ForeignKey('tweets.id'))
+    is_active = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    
     user = relationship("Users", back_populates="comments")
     tweet = relationship("Tweets", back_populates="comments")
     
@@ -50,18 +64,30 @@ class Comments(Base):
 """ Association tables """
 class TweetLikes(Base):
     __tablename__ = 'tweet_likes'
+    
     user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     tweet_id = Column(Integer, ForeignKey('tweets.id'), primary_key=True)
+    is_active = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
      
 
 class CommentLikes(Base):
     __tablename__ = 'comment_likes'
+    
     user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     comment_id = Column(Integer, ForeignKey('comments.id'), primary_key=True)
+    is_active = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
 
 
 class Follows(Base):
     # A follower follows the followee
     __tablename__ = 'follows'
+    
     follower_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     followee_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    is_active = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
