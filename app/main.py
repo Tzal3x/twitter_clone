62fbf18e17996e774511b1de0669c60d80792ec6
Alexandros Tzimas <tzalex@outlook.com.gr>
2023-06-07 15:55:48 +0300

    Add follow/unfollow endpoints

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+"""Starting point and sub-paths are defined here"""
 from fastapi import FastAPI
-from app.routers import users, login
+from app.routers import users, login, follows
+
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(login.router)
+app.include_router(follows.router)
+
 
 @app.get('/')
 def root():
+    """Root path"""
     return { "message": "Welcome to our Twitter Clone! Go to '/docs' for the API documentation." }
--- app/routers/follows.py
+++ app/routers/follows.py
@@ -1,4 +1,16 @@
-from fastapi import APIRouter
+"""
+Endpoint regarding the following mechanism of twitter.
+"""
+from typing import Annotated
+from fastapi import APIRouter, Query, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import exc
+
+from app.database import get_db
+from app.security import authorize_user
+from app.models import Users, Follows
+from app.queries import Queries
+
 
 router = APIRouter(
     prefix="/follows",
@@ -6,25 +18,47 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-""" List followers per user """
-@router.get()
-def list_followers():
-    pass
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def follow(username: Annotated[str, Query],
+           user: Users = Depends(authorize_user),
+           db: Session = Depends(get_db)):
+    """Command to follow a user"""
+    followee: Users = Queries.get_user(db, username)
+    new_follow_entry = Follows(
+            follower_id = user.id,
+            followee_id = followee.id
+        )
+    try:
+        db.add(new_follow_entry)
+        db.commit()
+    except exc.IntegrityError:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail="User has been already followed.")
 
 
-""" List following per user """
-@router.get()
-def list_following():
-    pass
+@router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
+def unfollow(username: Annotated[str, Query],
+             user: Users = Depends(authorize_user),
+             db: Session = Depends(get_db)):
+    """Unfollow user"""
+    followee: Users = Queries.get_user(db, username)
+    try:
+        db.query(Follows).filter(Follows.follower_id == user.id,
+                                 Follows.followee_id == followee.id).delete()
+        db.commit()
+    except exc.NoResultFound:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail="Not following.")
 
 
-""" Follow user """
-@router.post()
-def follow():
-    pass
+# @router.get("/followers")
+# def get_followers(username: Annotated[str, Query]):
+#     """Get a collection of the followers of a user"""
+#     pass
 
 
-""" Unfollow user """
-@router.delete()
-def unfollow():
-    pass
+# @router.get("/following")
+# def get_following(username: Annotated[str, Query]):
+#     """Get a collection of the users following a user"""
+#     pass
