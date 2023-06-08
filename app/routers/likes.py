93f9bcd1985507c2b42326cddc5a1d8ef2674e83
georgesouliotis <george_astrobip96@hotmail.com>
2023-06-08 21:40:07 +0300

    Complete like on tweets / comments

--- app/routers/likes.py
+++ app/routers/likes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, status, HTTPException, Depends, Response
-from app.models import TweetLikes, CommentLikes, Tweets
+from app.models import TweetLikes, CommentLikes, Tweets, Comments
 from app.schemas import UserReturn
 from sqlalchemy.orm import Session
 from sqlalchemy import exc
@@ -70,10 +70,50 @@ def remove_like_from_tweet(id: int,
 
 
 # """ Likes on comments """
-# @router.post()
-def add_like_to_comment():
-    pass
+@router.post('/comment/{id}', status_code=status.HTTP_201_CREATED)
+def add_like_to_comment(id: int, 
+                        current_user: Annotated[UserReturn, Depends(authorize_user)],
+                        db: Annotated[Session, Depends(get_db)]):
+    
+    like_on_comment = CommentLikes(comment_id=id, user_id=current_user.id)
+    try:
+        db.add(like_on_comment)
+        db.commit()
+        db.refresh(like_on_comment)
+    except exc.SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f"User with id: {current_user.id} has already liked comment with id: {id}, or comment does not exist.")
+        
+    return Response(status_code=status.HTTP_201_CREATED)
 
-# @router.delete()
-def remove_like_from_comment():
-    pass
+
+@router.delete('/comment/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def remove_like_from_comment(id: int,
+                             current_user: Annotated[UserReturn, Depends(authorize_user)],
+                             db: Annotated[Session, Depends(get_db)]):
+    
+    comment_query = db.query(Comments).filter(Comments.id == id)
+
+    comment = comment_query.first()
+
+    if comment == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Comment with id: {id} does not exist")
+
+    if comment.user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
+
+    like_on_comment_query = db.query(CommentLikes).filter(CommentLikes.comment_id == id, CommentLikes.user_id == current_user.id)
+
+    like_on_tweet = like_on_comment_query.first()
+
+    if like_on_tweet == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id: {current_user.id} never liked this comment in the first place")
+    
+    like_on_comment_query.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
